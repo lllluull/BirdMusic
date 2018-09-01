fce7a09a1aac0f5d339ff33110a167e9bf403c6f
hupengpeng <hupengpeng12345@163.com>
2018-09-01 08:47:30 +0800

    新的项目头部完成

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <mheader></mheader>
+
+  </div>
+</template>
+
+<script>
+import mheader from './components/header/header'
+export default {
+  name: 'App',
+  components: {
+    mheader
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='header'>
+    <div>
+      <div class="img">
+      </div>
+      <div class='title'>BirdMusic</div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="stylus" scoped>
+@import '~common/styl/variable'
+.header
+  height 1.2rem
+  background-color:red
+  text-align center
+  position relative
+  .title
+    height 1.2rem
+    line-height 1.2rem
+    display inline-block
+    vertical-align top
+    font-size $font-size-large-x
+  .img
+    display inline-block
+    background-image url('../../common/img/logo.png')
+    height .8rem
+    width .8rem
+    background-size .8rem .8rem
+    margin-top .2rem
+</style>
